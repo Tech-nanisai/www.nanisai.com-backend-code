ef602afec80d5d7b77bd6bf5695829d2ccdb9480
Tech-nanisai <techaettarisai@gmail.com>
2025-02-17 12:20:46 +0530

    Updated .env and Clint_register_routes.js

--- Routes/Clint_register_routes.js
+++ Routes/Clint_register_routes.js
@@ -14,22 +14,20 @@ router.post("/register", async (req, res) => {
 
         // Check if user already exists
         const existingClient = await ClintRegister.findOne({ email });
-        if (existingClient) {
-            return res.status(400).json({ message: "User already exists" });
-        }
+        if (existingClient) return res.status(400).json({ message: "User already exists" });
 
         // Hash password
-        const hashedPassword = await bcrypt.hash(password, 10);
+        const salt = await bcrypt.genSalt(10);
+        const hashedPassword = await bcrypt.hash(password, salt);
 
         // Save new user
         const newClient = new ClintRegister({ fullName, phoneNumber, email, password: hashedPassword });
         await newClient.save();
 
         // Generate JWT Token
-        const token = jwt.sign({ userId: newClient._id }, process.env.CLINT_JWT_SECRET, { expiresIn: "15d" });
+        const token = jwt.sign({ userId: newClient._id }, process.env.JWT_SECRET, { expiresIn: "15d" });
 
         res.status(201).json({ message: "User registered successfully", token });
-
     } catch (error) {
         res.status(500).json({ message: "Server Error", error: error.message });
     }
